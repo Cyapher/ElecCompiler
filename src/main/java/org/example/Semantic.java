be21d6725f1191acc96916c1f80bbb5d97839103
NeilVillarey <neiljoey.villarey.cics@ust.edu.ph>
2023-06-02 11:13:20 +0800

    Assign variables
    Assign variables

--- src/main/java/org/example/Semantic.java
+++ src/main/java/org/example/Semantic.java
@@ -10,59 +10,95 @@ public class Semantic {
     private List<SimpleEntry<String, String>> tokens; // value = type
     private TreeNode parseTree;
     private JTable symbolTable;
+    private Map<String, String> symbolValues; // Stores identifier values
+    private Map<String, Integer> identifierIndices; // Stores identifier indices in array
 
     public Semantic(List<SimpleEntry<String, String>> tokens, TreeNode parseTree) {
         this.tokens = tokens;
         this.parseTree = parseTree;
+        this.symbolValues = new HashMap<>();
+        this.identifierIndices = new HashMap<>();
     }
 
     public void Semantic() {
         System.out.println("============ IN SEMANTIC ============");
 
-        String[] columnNames = { "Index", "Token", "Type", "Value", "Reference" };
+        String[] columnNames = { "Index", "Token", "TokenType", "Value", "Reference" };
         String[][] array = new String[tokens.size()][5];
-        
-        // System.out.println("arr val" + array[0][1]);
 
         for (int i = 0; i < tokens.size(); i++) {
             SimpleEntry<String, String> entry = tokens.get(i);
-            for (int j = 0; j < 5; j++) {
-                if (j == 0) 
-                    array[i][j] = Integer.toString(i); //Token Index
-                else if (j == 1) 
-                    array[i][j] = entry.getKey(); //Token Lexeme
-                else if(j == 2)
-                    array[i][j] = entry.getValue(); //Token Type
-                else if(j == 3){  
-                    int pointer = i;                  //Token Value 
-                    if(entry.getValue() != "IDENTIFIER"){
-                        array[i][j] = entry.getKey();
-                    } else if(entry.getValue() == "IDENTIFIER" && array[pointer++][j] == "ASSIGNMENT"){
-                        // array[i][j] = array[][]
-                        System.out.println("JASKDHKSDHAKDSAKDHJAHDAKJHDASKJASDJKHASD HATTTTDOOOGGGG TANIGNA");
+            if (entry.getValue().equals("IDENTIFIER")) {
+                String identifier = entry.getKey();
+                if (identifierIndices.containsKey(identifier)) {
+                    if (tokens.get(i + 1).getValue().equals("ASSIGNMENT")) {
+                        String value;
+                        if (tokens.get(i + 2).getValue().equals("STRING_VALUE")) {
+                            value = "\"" + tokens.get(i + 2).getKey() + "\"";  // Preserve quotes for string values
+                        } else {
+                            value = tokens.get(i + 2).getKey();
+                        }
+                        array[identifierIndices.get(identifier)][3] = value; // Update value in array
+                        symbolValues.put(identifier, value); // Update value in symbol table
+                        array[i] = new String[] { Integer.toString(i), identifier + " - Reassignment", "IDENTIFIER", value, null };
+                        i += 2; // Skip assignment and value tokens
                     }
-                } else{ //if identifier
-                    
+                } else {
+                    identifierIndices.put(identifier, i);
+                    array[i][0] = Integer.toString(i);
+                    array[i][1] = identifier;
+                    array[i][2] = entry.getValue();
+                    if (tokens.get(i + 1).getValue().equals("ASSIGNMENT")) {
+                        String value;
+                        if (tokens.get(i + 2).getValue().equals("STRING_VALUE")) {
+                            value = "\"" + tokens.get(i + 2).getKey() + "\"";  // Preserve quotes for string values
+                        } else {
+                            value = tokens.get(i + 2).getKey();
+                        }
+                        array[i][3] = value;
+                        symbolValues.put(identifier, value);
+                    } else {
+                        array[i][3] = "<unassigned>";
+                    }
                 }
-                    
+            } else {
+                array[i][0] = Integer.toString(i);
+                array[i][1] = entry.getKey();
+                array[i][2] = entry.getValue();
+                array[i][3] = entry.getKey();
             }
+            // handle reference column logic here if needed
         }
+
 
         System.out.println("Token List: \n");
         for (String[] row : array) {
             for (String element : row) {
-                System.out.println(element + "\t");
+                System.out.print(element + "\t");
             }
-            System.out.println("\n");
+            System.out.println();
         }
 
-        //symbolTable = new JTable(array, columnNames);
-        SampleTable st = new SampleTable(array, columnNames);
-        st.createAndShowGUI();
-
+        System.out.println("\nToken List as Table: \n");
+        printTable(array, columnNames);
     }
-    
-   
 
+    private void printTable(String[][] data, String[] columnNames) {
+        int numRows = data.length;
+        int numColumns = columnNames.length;
 
+        // Print column names
+        for (String columnName : columnNames) {
+            System.out.print(columnName + "\t");
+        }
+        System.out.println();
+
+        // Print table data
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numColumns; j++) {
+                System.out.print(data[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
